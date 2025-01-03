a71b891b7332e9c4b9ad43c7eec6e5224167c11b
unknown <hrishikeshblaster@gmail.com>
2025-01-03 20:48:00 +0530

    Initial Commit

---
+++ models/properties-model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(async () => {
+    console.log("Database connected!");
+    console.log("MongoDB URI:", process.env.MONGO_URI);
+  })
+  .catch((err) => console.error("Database connection error:", err));
+
+const PropertySchema = new mongoose.Schema({
+  title: { type: String, required: true }, // Property title/name
+                                                       
+  mainCategory: { 
+    type: String, 
+    enum: ["Residential", "Commercial", "OffPlan"], 
+    required: true 
+  }, // Main category
+  subCategory: { 
+    type: String,
+     enum: { 
+      values: ['Apartment', 'Bungalow', 'Palace', 'Plot','Shop', 'Studio'],
+       message: 'Invalid subcategory for the selected category' }, 
+//        validate: { 
+//         validator: function(v) {
+//            if (this.category === 'Residential')
+//              { 
+//               return ['Apartment', 'Bungalow',].includes(v); 
+
+//             } else if (this.category === 'Commercial') { 
+
+//               return ['Shop', 'Studio'].includes(v);
+
+//             } else if (this.category === 'Off Plan') { 
+
+//               return ['Palace', 'Plots'].includes(v); 
+
+//             } else { 
+              
+//               return true;
+            
+//             } },
+  
+  
+//   message: 'Subcategory does not match the selected category'
+//  }
+},
+ // Sub-category
+  Images:[{type:String}],
+  bedrooms: { type: Number, required: true }, // Number of bedrooms
+  area: { type: Number, required: true }, // Number of bedrooms
+  location: { type: String, required: true }, // Location (used for "Keyword")
+  price: { type: Number, required: true }, // Property price
+  description : {type : String},
+  createdAt: { type: Date, default: Date.now }, // Creation date
+});
+
+module.exports = mongoose.model("Property", PropertySchema);
